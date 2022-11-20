0115dc9a9a8fe908e8a9c146ec097157cb37ab70
Victor <viktor.goryachev.00@mail.ru>
2022-11-20 09:24:53 +0300

    .

--- src/flights/pkg/handlers/flight.go
+++ src/flights/pkg/handlers/flight.go
@@ -25,36 +25,30 @@ func (h *FlightsHandler) GetAllFlight(w http.ResponseWriter, r *http.Request, ps
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Add("Content-Type", "application/json")
 	myjson.JsonResponce(w, http.StatusOK, flights)
 }
 
 func (h *FlightsHandler) GetFlight(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	number := ps.ByName("flightNumber")
-	log.Println(number)
 	flight, err := h.FlightRepo.GetFlightByNumber(number)
-
 	if err != nil {
 		log.Printf("Failed to get flights: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Add("Content-Type", "application/json")
 	myjson.JsonResponce(w, http.StatusOK, flight)
 }
 
 func (h *FlightsHandler) GetAirport(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	id := ps.ByName("airportID")
-	log.Println(id)
 	airport, err := h.AirportRepo.GetAirportByID(id)
 	if err != nil {
 		log.Printf("failed to get flghts: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Add("Content-Type", "application/json")
 	myjson.JsonResponce(w, http.StatusOK, airport)
 }
